245973d9dcef98e66b20ffbd637d385e8638dfd4
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2023-03-09 11:44:08 +0100

    Add support for setting R option 'tracker.debug' via environment variable 'R_TRACKERS_DEBUG' on load

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,12 @@
-.onLoad <- function(...) {
+.onLoad <- function(libname, pkgname) {
+  debug <- getOption("trackers.debug", NULL)
+  if (is.null(debug)) {
+    debug <- Sys.getenv("R_TRACKERS_DEBUG", NA_character_)
+    if (!is.na(debug)) {
+      options(trackers.debug = isTRUE(as.logical(debug)))
+    }
+  }
+    
   track_files(action = "init")
   track_locale(action = "init")
 }
